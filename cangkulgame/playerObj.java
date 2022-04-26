d0c86b3e954d04513490f7be05d6a49a5b093789
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2022-04-26 13:48:39 -0700

    made constructor method and other fields in the playerObject

--- cangkulgame/playerObj.java
+++ cangkulgame/playerObj.java
@@ -1,4 +1,15 @@
 package cangkulgame;
 
+import java.util.ArrayList; 
 public class playerObj {
+  private ArrayList<cardObj> playerHand = new ArrayList<cardObj>();  
+  private int playerNumber; 
+
+  public playerObj(int playerNumber){
+    this.playerNumber = playerNumber; 
+  } 
+
+  
+
+
 }
