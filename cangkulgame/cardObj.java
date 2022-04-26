8ddc45c93dd577fdee4f2cb2f9414b8ada549496
Tsaqif Insani <Tsaqif.Insani@gmail.com>
2022-04-26 13:43:52 -0700

    changed cardObj.java to add methods for the card object

--- cangkulgame/cardObj.java
+++ cangkulgame/cardObj.java
@@ -1,11 +1,22 @@
 package cangkulgame;
 
+
 public class cardObj {
-  private static int rank; 
-  private static String suit; 
+  private int rank; 
+  private String suit; 
 
   public cardObj(int rank, String suit){
     this.rank = rank;
     this.suit = suit;  
+  }
+
+  public String getSuitAndRank(){
+    String suitAndRank = "" + rank + " " + suit; 
+    return suitAndRank; 
+  }
+
+  public String toString(){
+    String cardInStringFormat = "rank: " + rank + ", suit: " + suit; 
+    return cardInStringFormat; 
   }
 }
